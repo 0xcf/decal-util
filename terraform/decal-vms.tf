277c2b672d4ca8a9b0ba4b992ad1741bf7b6c1b5
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-02-08 03:58:18 -0800

    ./terraform fmt

--- terraform/decal-vms.tf
+++ terraform/decal-vms.tf
@@ -2,50 +2,59 @@ variable "do_token" {}
 variable "dnsimple_token" {}
 variable "dnsimple_account" {}
 
-variable "decal_ssh_fingerprint" { type = "list" }
+variable "decal_ssh_fingerprint" {
+  type = "list"
+}
 
-variable "students" { type = "list" }
+variable "students" {
+  type = "list"
+}
 
-variable "berkeley_subnets" { type = "list" }
-variable "berkeley_subnets6" { type = "list" }
+variable "berkeley_subnets" {
+  type = "list"
+}
+
+variable "berkeley_subnets6" {
+  type = "list"
+}
 
 provider "digitalocean" {
-    token = "${ var.do_token }"
+  token = "${ var.do_token }"
 }
 
 provider "dnsimple" {
-    token   = "${ var.dnsimple_token }"
-    account = "${ var.dnsimple_account }"
+  token   = "${ var.dnsimple_token }"
+  account = "${ var.dnsimple_account }"
 }
 
 resource "digitalocean_tag" "staff" {
-    name = "staff"
+  name = "staff"
 }
 
 resource "digitalocean_tag" "student" {
-    name = "student"
+  name = "student"
 }
 
 resource "digitalocean_firewall" "student_firewall" {
-    name = "only-uc-berkeley"
-    tags = [ "${ digitalocean_tag.staff.id }", "${ digitalocean_tag.student.id }" ]
+  name = "only-uc-berkeley"
+  tags = ["${ digitalocean_tag.staff.id }", "${ digitalocean_tag.student.id }"]
 
-    inbound_rule = [
-        {
-            protocol           = "tcp"
-            port_range         = "1-65535"
-            source_addresses   = "${ concat(var.berkeley_subnets, var.berkeley_subnets6) }"
-        },
-        {
-            protocol           = "udp"
-            port_range         = "1-65535"
-            source_addresses   = "${ concat(var.berkeley_subnets, var.berkeley_subnets6) }"
-        },
-        {
-            protocol           = "icmp"
-            source_addresses   = "${ concat(var.berkeley_subnets, var.berkeley_subnets6) }"
-        }
-    ]
+  inbound_rule = [
+    {
+      protocol         = "tcp"
+      port_range       = "1-65535"
+      source_addresses = "${ concat(var.berkeley_subnets, var.berkeley_subnets6) }"
+    },
+    {
+      protocol         = "udp"
+      port_range       = "1-65535"
+      source_addresses = "${ concat(var.berkeley_subnets, var.berkeley_subnets6) }"
+    },
+    {
+      protocol         = "icmp"
+      source_addresses = "${ concat(var.berkeley_subnets, var.berkeley_subnets6) }"
+    },
+  ]
 }
 
 resource "digitalocean_droplet" "staff" {
@@ -54,12 +63,11 @@ resource "digitalocean_droplet" "staff" {
   region             = "sfo2"
   size               = "1gb"
   private_networking = "true"
-  ssh_keys           = [ "${ var.decal_ssh_fingerprint }" ]
-  tags               = [ "${ digitalocean_tag.staff.id }" ]
+  ssh_keys           = ["${ var.decal_ssh_fingerprint }"]
+  tags               = ["${ digitalocean_tag.staff.id }"]
 }
 
 resource "digitalocean_droplet" "students" {
-
   count = "${ length(var.students) }"
 
   image              = "debian-9-x64"
@@ -67,9 +75,8 @@ resource "digitalocean_droplet" "students" {
   region             = "sfo2"
   size               = "1gb"
   private_networking = "true"
-  ssh_keys           = [ "${ var.decal_ssh_fingerprint }" ]
-  tags               = [ "${ digitalocean_tag.student.id }" ]
-
+  ssh_keys           = ["${ var.decal_ssh_fingerprint }"]
+  tags               = ["${ digitalocean_tag.student.id }"]
 }
 
 output "staff_ip" {
