d11b11cb0fbda9f4402db1ea7bb61f2ce3d87b60
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-02-18 14:13:31 -0800

    firewall: whitelist all VMs to one another

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -6,17 +6,17 @@ resource "digitalocean_firewall" "student_firewall" {
     {
       protocol         = "tcp"
       port_range       = "1-65535"
-      source_addresses = "${ concat(var.berkeley_subnets, var.berkeley_subnets6, var.internal_subnet) }"
+      source_addresses = ["${ var.berkeley_subnets }", "${ var.berkeley_subnets6 }", "${ var.internal_subnet }", "${ digitalocean_droplet.students.*.ipv4_address }", "${ digitalocean_droplet.advanced_students.*.ipv4_address}", "${ digitalocean_droplet.staff.ipv4_address }", "${ digitalocean_droplet.test.ipv4_address }"]
     },
     {
       protocol         = "udp"
       port_range       = "1-65535"
-      source_addresses = "${ concat(var.berkeley_subnets, var.berkeley_subnets6, var.internal_subnet) }"
+      source_addresses = ["${ var.berkeley_subnets }", "${ var.berkeley_subnets6 }", "${ var.internal_subnet }", "${ digitalocean_droplet.students.*.ipv4_address }", "${ digitalocean_droplet.advanced_students.*.ipv4_address}", "${ digitalocean_droplet.staff.ipv4_address }", "${ digitalocean_droplet.test.ipv4_address }"]
     },
     {
       protocol         = "icmp"
       port_range       = ""
-      source_addresses = "${ concat(var.berkeley_subnets, var.berkeley_subnets6, var.internal_subnet) }"
+      source_addresses = ["${ var.berkeley_subnets }", "${ var.berkeley_subnets6 }", "${ var.internal_subnet }", "${ digitalocean_droplet.students.*.ipv4_address }", "${ digitalocean_droplet.advanced_students.*.ipv4_address}", "${ digitalocean_droplet.staff.ipv4_address }", "${ digitalocean_droplet.test.ipv4_address }"]
     },
   ]
 
