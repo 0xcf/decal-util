f2fe727b8e9c951f8fbe2c569c6fac4a253959cb
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-02-17 19:15:19 -0800

    fix terraform resource variable name :(

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -12,7 +12,7 @@ resource "dnsimple_record" "advanced-vms" {
   count = "${ length(var.advanced_students) }"
 
   domain = "xcf.sh"
-  name   = "${ element(var.students, count.index) }.decal"
+  name   = "${ element(var.advanced_students, count.index) }.decal"
   type   = "A"
   ttl    = 3600
   value  = "${ element(digitalocean_droplet.advanced_students.*.ipv4_address, count.index) }"
